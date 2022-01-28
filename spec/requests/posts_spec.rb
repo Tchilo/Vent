3e36d44269cea77f5e3c071d722225c874ae59da
Tchilo <tchiloross@gmail.com>
2022-01-29 00:14:18 +0200

    Remove rspec-rails from gem file due to duplicat

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,36 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
-  it 'should have a status code of 200 ' do
-    get users_path
-    expect(response).to have_http_status(200)
+    it 'should have a status code of 200 ' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should have a template of show' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'should have place holder of Users#index' do
+      get users_path
+      expect(response.body).to include('Users#index')
+    end
   end
 
-  it 'should have a template of show' do
-    get users_path
-    expect(response).to render_template(:index)
-end
+  describe 'GET #show' do
+    before(:example) { get user_path(1) }
 
-it 'should have place holder of Users#index' do
-  get users_path
-  expect(response.body).to include('Users#index')
-end
-end
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
 
-describe 'GET #show' do
-before(:example) { get user_path(1) }
+    it 'should render correct template for show' do
+      expect(response).to render_template(:show)
+    end
 
-it 'should have correct response status' do
-  expect(response).to have_http_status(:ok)
-end
-
-it 'should render correct template for show' do
-  expect(response).to render_template(:show)
-end
-
-it 'should include show for users placeholder text' do
-  expect(response.body).to include('Users/show')
-end
-
-end
+    it 'should include show for users placeholder text' do
+      expect(response.body).to include('Users/show')
+    end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET#index' do
-   it 'should have a status of 200' do
-    get users_path
-    expect(response).to have_http_status(200)
-   end
+    it 'should have a status of 200' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
 
-   it 'should show template index' do
-    get users_path
-    expect(response).to render_template(:index)
-   end
+    it 'should show template index' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
 
-   it 'should have content on page that says -show for users' do
-    get users_path
-    expect(response.body).to include('Users#index')
-   end
+    it 'should have content on page that says -show for users' do
+      get users_path
+      expect(response.body).to include('Users#index')
+    end
   end
   describe 'GET #show' do
     before(:example) { get user_path(1) }
